6585f39584fb86ca7ffe60057c15f5e0715ce91e
keitaroskmt <keitaroskmt@gmail.com>
2024-11-12 16:13:57 +0900

    Add support for HF image dataset

--- src/datasets/cifar.py
+++ src/datasets/cifar.py
@@ -1,9 +1,16 @@
+import logging
+
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
+from datasets import Dataset as HFDataset
+from datasets import load_dataset
+from transformers import ViTImageProcessor
 
 from src.datasets.utils import add_label_noise
+
+logger = logging.getLogger(__name__)
 
 
 def get_cifar10_datasets(
@@ -35,4 +42,37 @@ def get_cifar10_datasets(
             train_dataset.targets, noise_ratio, num_classes=10
         )
 
+    return train_dataset, test_dataset
+
+
+def get_cifar10_hf_datasets(
+    processor: ViTImageProcessor,
+    noise_ratio: float = 0.0,
+) -> tuple[HFDataset, HFDataset]:
+    raw_datasets = load_dataset("cifar10")
+    raw_datasets = raw_datasets.rename_column("img", "pixel_values")
+
+    def transform(example_batch):
+        inputs = processor(example_batch["pixel_values"], return_tensors="pt")
+        inputs["label"] = torch.tensor(example_batch["label"])
+        return inputs
+
+    raw_datasets.set_transform(transform)
+
+    train_dataset = raw_datasets["train"]
+    test_dataset = raw_datasets["test"]
+
+    logger.info(f"Key names in the dataset: {train_dataset.column_names}.")
+    logger.info(
+        f"Types of input and target are : {type(train_dataset[0]["pixel_values"]), type(train_dataset[0]["label"])}."
+    )
+    logger.info(f"Size of input tensor is : {train_dataset[0]['pixel_values'].shape}.")
+
+    if noise_ratio > 0.0:
+        train_dataset = train_dataset.map(
+            lambda example: {
+                "label": add_label_noise(example["label"], noise_ratio, num_classes=10)
+            },
+            features=train_dataset.features,
+        )
     return train_dataset, test_dataset
--- src/datasets/mnist.py
+++ src/datasets/mnist.py
@@ -1,9 +1,18 @@
+import logging
+
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.datasets import MNIST
 
+from datasets import Dataset as HFDataset
+from datasets import load_dataset
+from transformers import ViTImageProcessor
+
 from src.datasets.utils import add_label_noise
+
+logger = logging.getLogger(__name__)
 
 
 # Following the experimental setup in the paper:
@@ -42,4 +51,47 @@ def get_mnist_snr_datasets(
             train_dataset.targets, noise_ratio, num_classes=10
         )
 
+    return train_dataset, test_dataset
+
+
+def get_mnist_snr_hf_datasets(
+    processor: ViTImageProcessor,
+    noise_ratio: float = 0.0,
+    snr: float = 1.0,
+) -> tuple[HFDataset, HFDataset]:
+    raw_datasets = load_dataset("mnist")
+    raw_datasets = raw_datasets.rename_column("image", "pixel_values")
+
+    def transform(example_batch):
+        # Reshape images from 2D (H, W) to 3D (H, W, C) and convert to 3-channels
+        # Also, normalize pixel values to [0, 1]
+        images = []
+        for image in example_batch["pixel_values"]:
+            image = np.repeat(np.reshape(image, (28, 28, 1)) / 255, 3, axis=2)
+            noise = np.repeat(np.random.randn(28, 28, 1), 3, axis=2)
+            noise[5:23, 5:23, :] = 0.0
+            image = image * snr + noise
+            images.append((image - np.min(image)) / (np.max(image) - np.min(image)))
+        inputs = processor(images, do_rescale=False, return_tensors="pt")
+        inputs["label"] = torch.tensor(example_batch["label"])
+        return inputs
+
+    raw_datasets.set_transform(transform)
+    train_dataset = raw_datasets["train"]
+    test_dataset = raw_datasets["test"]
+
+    logger.info(f"Key names in the dataset: {train_dataset.column_names}.")
+    logger.info(
+        f"Types of input and target are : {type(train_dataset[0]["pixel_values"]), type(train_dataset[0]["label"])}."
+    )
+    logger.info(f"Size of input tensor is : {train_dataset[0]['pixel_values'].shape}.")
+
+    noise_ratio = 0.0
+    if noise_ratio > 0.0:
+        train_dataset = train_dataset.map(
+            lambda example: {
+                "label": add_label_noise(example["label"], noise_ratio, num_classes=10)
+            },
+            features=train_dataset.features,
+        )
     return train_dataset, test_dataset
