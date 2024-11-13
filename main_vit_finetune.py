2bf82a406bb097d295fab626269818a2e2ece85a
keitaroskmt <keitaroskmt@gmail.com>
2024-11-13 18:44:16 +0900

    Add codes for experiments

---
+++ main_vit_finetune.py
@@ -0,0 +1,151 @@
+import os
+import logging
+
+import wandb
+import hydra
+import torch
+import torch.distributed as dist
+from omegaconf import DictConfig, OmegaConf
+
+from transformers import (
+    ViTImageProcessor,
+    ViTForImageClassification,
+    Trainer,
+    TrainingArguments,
+)
+from src.datasets.mnist import get_mnist_snr_hf_datasets_for_finetune
+from src.datasets.cifar import get_cifar10_hf_datasets_for_finetune
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+@hydra.main(config_path="config", config_name="main_vit", version_base=None)
+def main(cfg: DictConfig) -> None:
+    if not cfg["use_ddp"] or (cfg["use_ddp"] and os.environ.get("RANK", "0") == 0):
+        wandb_config = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+        wandb.init(project="benign_attention_vit_finetune", config=wandb_config)
+
+    seed = 0
+    torch.manual_seed(seed)
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
+
+    if cfg["use_ddp"]:
+        dist.init_process_group("nccl")
+
+    processor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k")
+    dataset_name = cfg["dataset"]["name"]
+    if dataset_name == "cifar10":
+        pretrain_dataset, finetune_dataset, test_dataset = (
+            get_cifar10_hf_datasets_for_finetune(
+                processor=processor,
+                pretrain_sample_size=cfg["pretrain_sample_size"],
+                sample_size=cfg["sample_size"],
+                noise_ratio=cfg["noise_ratio"],
+            )
+        )
+    elif dataset_name == "mnist_snr":
+        pretrain_dataset, finetune_dataset, test_dataset = (
+            get_mnist_snr_hf_datasets_for_finetune(
+                processor=processor,
+                pretrain_sample_size=cfg["pretrain_sample_size"],
+                sample_size=cfg["sample_size"],
+                snr=1.0,
+                noise_ratio=cfg["noise_ratio"],
+            )
+        )
+    else:
+        raise NotImplementedError(f"Dataset {dataset_name} is not supported.")
+
+    model = ViTForImageClassification.from_pretrained(
+        "google/vit-base-patch16-224-in21k",
+        num_labels=10,
+        attention_probs_dropout_prob=0.0,
+        hidden_dropout_prob=0.0,
+    )
+    model = model.to(device)
+
+    def compute_metrics(eval_pred):
+        logits = (
+            eval_pred.predictions[0]
+            if isinstance(eval_pred.predictions, tuple)
+            else eval_pred.predictions
+        )
+        labels = eval_pred.label_ids
+        return {"accuracy": (logits.argmax(-1) == labels).mean()}
+
+    logger.info(f"Config: {cfg}")
+
+    ##### First training phase: pretrain the model without label noise #####
+    logger.info("#############################################################")
+    logger.info("First training phase: pretraining the model without label noise.")
+
+    # Freeze all layers except the last classifier.
+    for name, param in model.named_parameters():
+        if name.startswith("classifier"):
+            param.requires_grad = True
+        else:
+            param.requires_grad = False
+
+    training_args = TrainingArguments(
+        output_dir="./results",
+        num_train_epochs=cfg["pretrain_num_epochs"],
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=64,
+        logging_dir="./logs",
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=pretrain_dataset,
+        eval_dataset=test_dataset,
+        compute_metrics=compute_metrics,
+    )
+    trainer.train()
+    result = trainer.evaluate()
+    logger.info(result)
+    trainer.save_state()
+
+    ##### Second training phase: finetune the model with label noise #####
+    logger.info("#############################################################")
+    logger.info("Second training phase: finetuning the model with label noise.")
+    for name, param in model.named_parameters():
+        if name.startswith("vit.encoder.layer.11.attention.attention"):
+            param.requires_grad = True
+        else:
+            param.requires_grad = False
+
+    training_args = TrainingArguments(
+        output_dir="./results",
+        num_train_epochs=cfg["num_epochs"],
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=64,
+        logging_dir="./logs",
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=finetune_dataset,
+        eval_dataset=test_dataset,
+        compute_metrics=compute_metrics,
+    )
+    trainer.train()
+    result = trainer.evaluate()
+    logger.info(result)
+    trainer.save_state()
+
+    if cfg["use_ddp"]:
+        dist.destroy_process_group()
+
+    if not cfg["use_ddp"] or (cfg["use_ddp"] and os.environ.get("RANK", "0") == 0):
+        wandb.finish()
+
+
+if __name__ == "__main__":
+    main()
--- src/datasets/agnews.py
+++ src/datasets/agnews.py
@@ -3,6 +3,7 @@
 import logging
 import random
 
+import numpy as np
 from datasets import load_dataset, Dataset
 from transformers import AutoTokenizer
 
@@ -77,3 +78,76 @@ def get_agnews_datasets(
         )
 
     return train_dataset, test_dataset
+
+
+# Used in the experiment in `main_bert_finetune.py`
+def get_agnews_datasets_for_finetune(
+    pretrain_sample_size: int,
+    sample_size: int,
+    noise_ratio: float = 0.0,
+    model_name_or_path: str | None = "bert-base-uncased",
+    pad_to_max_length: bool = True,
+) -> tuple[Dataset, Dataset, Dataset]:
+    raw_datasets = load_dataset("fancyzhx/ag_news", cache_dir="~/pytorch_datasets")
+    max_seq_length = 128
+    label_list = raw_datasets["train"].features["label"].names
+    num_labels = len(label_list)
+
+    if model_name_or_path:
+        tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+    else:
+        raise NotImplementedError("Only model_name_or_path is supported for now.")
+
+    # Padding strategy
+    if pad_to_max_length:
+        padding = "max_length"
+    else:
+        padding = "longest"
+
+    def preprocess_function(examples):
+        return tokenizer(
+            examples["text"],
+            padding=padding,
+            truncation=True,
+            max_length=max_seq_length,
+            add_special_tokens=False,
+            return_tensors="pt",
+        )
+
+    with main_process_first(desc="dataset map pre-processing"):
+        raw_datasets = raw_datasets.map(
+            preprocess_function, batched=True, desc="Running tokenizer on dataset"
+        )
+        raw_datasets = raw_datasets.remove_columns(["text", "token_type_ids"])
+        raw_datasets.set_format(type="torch")
+
+    train_dataset = raw_datasets["train"]
+    test_dataset = raw_datasets["test"]
+
+    assert pretrain_sample_size + sample_size <= len(
+        train_dataset
+    ), "Sample size is too large."
+
+    random_indices = np.random.choice(
+        len(train_dataset), pretrain_sample_size + sample_size, replace=False
+    )
+    pretrain_dataset = train_dataset.select(random_indices[:pretrain_sample_size])
+    finetune_dataset = train_dataset.select(random_indices[pretrain_sample_size:])
+
+    # Log a few random samples from the training set:
+    for index in random.sample(range(len(train_dataset)), 1):
+        logger.info(f"Number of classes in the dataset: {num_labels}.")
+        logger.info(f"Sample {index} of the training set: {train_dataset[index]}.")
+
+    # Add label noise only to the finetune_dataset
+    noise_ratio = 0.0
+    if noise_ratio > 0.0:
+        finetune_dataset = finetune_dataset.map(
+            lambda example: {
+                "label": add_label_noise(
+                    example["label"], noise_ratio, num_classes=num_labels
+                )
+            },
+            features=finetune_dataset.features,
+        )
+    return pretrain_dataset, finetune_dataset, test_dataset
